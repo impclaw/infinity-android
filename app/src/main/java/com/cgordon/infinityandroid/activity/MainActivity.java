df95d7c0bf5a127350bf9bed574814346828c820
CG <cgordon.13@gmail.com>
2015-05-26 10:02:21 -0400

    Add Toolbar to the app.  Not sure if it's necessary on the Army screen, but it should be more useful on other screens and I think it's odd to have it appearing and disappearing.

--- app/src/main/java/com/cgordon/infinityandroid/activity/MainActivity.java
+++ app/src/main/java/com/cgordon/infinityandroid/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.cgordon.infinityandroid.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -13,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
     }
 
