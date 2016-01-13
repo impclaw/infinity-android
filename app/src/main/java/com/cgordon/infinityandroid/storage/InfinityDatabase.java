b114d1d2623fab782ee05783ebf54cfd47000675
CG <cgordon.13@gmail.com>
2016-01-13 16:34:42 -0500

    Update 0.3.10 - Added Alguaciles to MRRF and Qapu Khalki

--- app/src/main/java/com/cgordon/infinityandroid/storage/InfinityDatabase.java
+++ app/src/main/java/com/cgordon/infinityandroid/storage/InfinityDatabase.java
@@ -32,7 +32,7 @@ import com.cgordon.infinityandroid.json.WeaponParser;
 public class InfinityDatabase extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "infinity.db";
-    private static final int DATABASE_VERSION = 18;
+    private static final int DATABASE_VERSION = 19;
 
     public static final String TABLE_WEAPONS = "weapons";
     public static final String TABLE_UNITS = "units";
