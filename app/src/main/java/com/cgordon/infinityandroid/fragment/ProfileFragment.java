01b4ef3ab7ee852fcde086bab0c819d209a05e16
CG <cgordon.13@gmail.com>
2016-02-04 19:12:06 -0500

    Updating copyright for all the code files.  No other change.

--- app/src/main/java/com/cgordon/infinityandroid/fragment/ProfileFragment.java
+++ app/src/main/java/com/cgordon/infinityandroid/fragment/ProfileFragment.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 by Chris Gordon
+ * Copyright 2015-2016 by Chris Gordon
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
