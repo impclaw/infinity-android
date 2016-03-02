892828ad1d2ee81014299d0efb54585d92be3c39
CG <cgordon.13@gmail.com>
2016-03-02 15:50:22 -0500

    Basic Drawer with Recycler View

--- app/src/main/java/com/cgordon/infinityandroid/adapter/SavedListsAdapter.java
+++ app/src/main/java/com/cgordon/infinityandroid/adapter/SavedListsAdapter.java
@@ -15,7 +15,32 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.cgordon.infinityandroid.data;
+package com.cgordon.infinityandroid.adapter;
 
-public interface ListElement {
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SavedListsAdapter  extends RecyclerView.Adapter<SavedListsAdapter.ViewHolder> {
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
--- app/src/main/java/com/cgordon/infinityandroid/fragment/SavedListsFragment.java
+++ app/src/main/java/com/cgordon/infinityandroid/fragment/SavedListsFragment.java
@@ -1,19 +1,43 @@
+/*
+ * Copyright 2016 by Chris Gordon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.cgordon.infinityandroid.fragment;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.cgordon.infinityandroid.R;
+import com.cgordon.infinityandroid.adapter.ArmyAdapter;
+import com.cgordon.infinityandroid.adapter.SavedListsAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SavedListsFragment extends Fragment {
 
+
+    private static final String TAG = SavedListsFragment.class.getSimpleName();
+    private RecyclerView m_recyclerView;
 
     public SavedListsFragment() {
         // Required empty public constructor
@@ -24,7 +48,19 @@ public class SavedListsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_saved_lists, container, false);
+        View view = inflater.inflate(R.layout.fragment_saved_lists, container, false);
+
+        m_recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+
+        m_recyclerView.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        m_recyclerView.setLayoutManager(layoutManager);
+        RecyclerView.Adapter adapter = new ArmyAdapter(getActivity());
+        m_recyclerView.setAdapter(new SavedListsAdapter());
+
+        return view;
+
     }
 
 
