0d823e7a66c2e21a86b2a6f74f6ee873f51190a2
CG <cgordon.13@gmail.com>
2016-03-13 15:29:01 -0400

    In future, don't delete the table of lists unless you have to.

--- app/src/main/java/com/cgordon/infinityandroid/storage/InfinityDatabase.java
+++ app/src/main/java/com/cgordon/infinityandroid/storage/InfinityDatabase.java
@@ -319,8 +319,8 @@ public class InfinityDatabase extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_PROFILES);
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_ARMY);
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_ARMY_UNITS);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ARMY_LISTS);
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ARMY_LIST_UNITS);
+//        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ARMY_LISTS);
+//        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ARMY_LIST_UNITS);
 
         onCreate(db);
     }
